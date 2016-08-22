c52ffc0417a4dea635db445cd6ad90d6fc32ad67
hermesagm <hermesagm@gmail.com>
2016-08-22 15:00:25 -0500

    Agrega validaciones a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@
 
 class User < ActiveRecord::Base
 	has_secure_password validation: false #Queda cifrado la contraseña
+	validates :email, uniqueness: true, format: /@/
+	validates :password, presence: true, on: :create
+	validates :password, length: { in: 6..20 }, allow_nil: true
+	validates :name, presence: true
 end
