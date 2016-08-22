4e6a9e2f900ac972345ae74e90d0ab2285436a1c
hermesagm <hermesagm@gmail.com>
2016-08-22 11:08:54 -0500

    Agregar la ruta root

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   #get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   #patch '/products/:id', to: 'products#update'
   #delete '/products/:id', to: 'products#destroy'
-  
+
   resources :products
+  root 'products#index' #Especifica la raiz del proudcto
 end
