d6aa05b7fb63bd308caa3be7b3edbe9557f860e5
hermesagm <hermesagm@gmail.com>
2016-08-24 11:29:18 -0500

    Implementa la funcionalidad de votos

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   #----------------------------------------------------------
   resources :users, only: [:new, :create] #El usuario solo tiene acceso a estos dos metodos
   resources :products do
+    resource :vote, only:[:create, :destroy]
     resources :comments, only: [:create]
   end
   
